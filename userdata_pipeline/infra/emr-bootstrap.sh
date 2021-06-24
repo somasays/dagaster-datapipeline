c430497b3bdeba3d8d0c9385834c0acb2e8c9276
Somasundaram Sekar <soma.sekar@thoughtworks.com>
2021-06-24 11:55:57 +0200

    with emr :)

--- userdata_pipeline/infra/emr-bootstrap.sh
+++ userdata_pipeline/infra/emr-bootstrap.sh
@@ -3,12 +3,18 @@
 # Purpose: EMR bootstrap script
 # Author:  Gary A. Stafford (2021-04-05)
 
+SET -e
+
 # update and install some useful yum packages
 sudo yum install -y jq
 
 # set region for boto3
 aws configure set region \
   "$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)"
+
+sudo python3 -m pip install --upgrade pip
+
+sudo python3 -m pip install --upgrade setuptools
 
 # install some useful python packages
 sudo python3 -m pip install \
